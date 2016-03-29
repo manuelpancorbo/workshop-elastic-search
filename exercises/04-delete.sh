b6bd49a7af5fb78bc1850aa4975f72469f2a0306
Odín del Río <odin.drp@gmail.com>
2016-03-29 23:09:34 +0200

    Delete example

---
+++ exercises/04-delete.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Location as array long/lat
+curl -XPUT "http://ws-elastic.dm:9200/ads/ad/5" -d'
+{
+    "id": 5,
+    "title": "Gonna be deleted",
+    "body": "...",
+    "location": [2.1660139, 41.3791979],
+    "zipCode": "08001"
+}'
+
+curl -XDELETE "http://ws-elastic.dm:9200/ads/ad/5"
