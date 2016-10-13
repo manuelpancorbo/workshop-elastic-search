814a6bbc2623987d0936cf86b5fa23ac2bf9931b
Odín del Río <odin.drp@gmail.com>
2016-10-13 22:16:08 +0200

    added alias management examples

--- exercises/01-create-ad-mapping.sh
+++ exercises/01-create-ad-mapping.sh
@@ -6,6 +6,30 @@ curl -XDELETE "http://localhost:9200/ads"
 # Create index
 curl -XPUT "http://localhost:9200/ads"
 
+# Add an alias
+curl -XPOST 'http://localhost:9200/_aliases' -d '
+{
+    "actions" : [
+        { "add" : { "index" : "ads", "alias" : "alias-for-ads" } }
+    ]
+}' | python -m json.tool
+
+# Short alias adding
+curl -XPUT 'http://localhost:9200/ads/_alias/old-ads'
+
+# Creating an index 2015 ads
+curl -XPUT "http://localhost:9200/ads-2015"
+
+# Changing the alias old-ads for pointing to the ads-2015 index.
+curl -XPOST 'http://localhost:9200/_aliases' -d '
+{
+    "actions" : [
+        { "remove" : { "index" : "ads", "alias" : "old-ads" } },
+        { "add" : { "index" : "ads-2015", "alias" : "old-ads" } }
+    ]
+}'
+
+
 # Create mapping
 curl -XPUT -d '
 {
@@ -40,3 +64,6 @@ curl -XPUT -d '
     }
 }
 ' "http://localhost:9200/ads/_mapping/ad" | python -m json.tool
+
+#Getting created mapping
+curl -XGET "http://localhost:9200/ads/_mapping/ad" | python -m json.tool
