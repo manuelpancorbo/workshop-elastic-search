4c4131bc85e1b1dffb36305f2613007ba4df43cd
Odín del Río <odin.drp@gmail.com>
2016-10-14 02:44:53 +0200

    added alternative way to combine queries and filters

--- exercises/10-queries-and-filters.sh
+++ exercises/10-queries-and-filters.sh
@@ -39,3 +39,25 @@ curl -XGET "http://localhost:9200/ads/ad/_search" -d '
   }
 }
 ' | python -m json.tool
+
+
+curl -XGET "http://localhost:9200/ads/ad/_search" -d '
+{
+  "query": {
+      "filtered": {
+  		"query" : {
+  			"term": {
+  				"title": "thing"
+  			}
+  		},
+	  	"filter" : {
+		    "range": {
+		      "price": {
+                "gte": 15,
+                "lte": 20.6
+		      }
+	        }
+	    }
+  	  }
+  }
+}' | python -m json.tool
