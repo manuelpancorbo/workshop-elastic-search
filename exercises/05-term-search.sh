65c1c332bef4097b7746858180cb262cbdabd195
Odín del Río <odin.drp@gmail.com>
2016-10-14 02:44:20 +0200

    added search for analyzed field

--- exercises/05-term-search.sh
+++ exercises/05-term-search.sh
@@ -6,6 +6,20 @@ curl -XGET "http://localhost:9200/ads/ad/_search" -d '
 {
   "query": {
     "term": {
+      "zipCode": "08001"
+    }
+  }
+}
+' | python -m json.tool
+
+# Short form for simple queries
+curl -XGET "http://localhost:9200/ads/ad/_search?q=zipCode:08001"
+
+# Term search for analyzed field
+curl -XGET "http://localhost:9200/ads/ad/_search" -d '
+{
+  "query": {
+    "term": {
       "title": "thing"
     }
   }
